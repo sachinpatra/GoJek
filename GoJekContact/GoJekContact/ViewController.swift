393b668edf54d554024bfa058c36e9ffb911363a
Sachin <skpatra@kirusa.com>
2019-02-21 23:52:08 +0530

    Project Setup

---
+++ GoJekContact/GoJekContact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoJekContact
+//
+//  Created by Sachin Kumar Patra on 2/21/19.
+//  Copyright © 2019 Sachin Kumar Patra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
